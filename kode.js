5ddf3c80a223fe43d51bddcc3c8bc271a4da728d
RezzziBOOM <pareziparhan2703@gmail.com>
2023-07-30 09:55:02 +0700

    Upload membuat text berjalan mengunakan javascript

---
+++ kode.js
@@ -0,0 +1,9 @@
+/*========== Tulisan Gerak gerak===========*/
+const typed = new Typed('.multiple-RezzziBOOM', {
+    strings: ['Cyber Security', 'Graphic Designer'],
+    typeSpeed: 80,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
